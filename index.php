d3057b8f18c2df82aef5d3febdaa64175eb38dd7
SBTHDR <subrata.halder13d@gmail.com>
2020-11-17 20:17:25 +0600

    Routing init

--- index.php
+++ index.php
@@ -1,9 +1,17 @@
 <?php
 
 require_once __DIR__.'/vendor/autoload.php';
+use app\core\Application;
 
 $app = new Application();
 
 $app->router->get('/', function() {
-    return 'Home route';
+    return 'Home Page';
 });
+
+$app->router->get('/contact', function() {
+    return 'Contact Page';
+});
+
+
+$app->run();
